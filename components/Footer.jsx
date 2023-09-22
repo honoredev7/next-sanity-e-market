0825f98bd1ec9fd2d687e0ac3493d9822c0ecd1b
honoré <honoreadj7@gmail.com>
2023-09-22 18:01:50 +0000

    correction of footer sentence

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>{ new Date().getFullYear() } E-Market, Tout droit reservé &copy;</p>
+      <p>{ new Date().getFullYear() } E-Market, Tout droit réservé &copy;</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
